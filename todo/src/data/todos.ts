5a98f471e02b7cadae1b0767408a7a9a22d6508f
nzhurat <zuratnasta2606@icloud.com>
2025-08-06 18:21:20 +0200

    add dummy data

---
+++ todo/src/data/todos.ts
@@ -0,0 +1,19 @@
+import type { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "Baking",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Walking",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Reading",
+        completed: false,
+    },
+];
