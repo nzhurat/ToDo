7d798761668ea0bc5fd3c6f53a87408de3eafbde
nzhurat <zuratnasta2606@icloud.com>
2025-08-06 18:40:13 +0200

    add an alert for completed todos

--- todo/src/components/TodoItem.tsx
+++ todo/src/components/TodoItem.tsx
@@ -2,14 +2,17 @@ import type { Todo } from "../types/todo";
 
 interface TodoItemProps {
     todo: Todo;
+    onCompletedChange: (id: number, completed: boolean) => void;
 }
 
-export default function TodoItem({todo}: TodoItemProps) {
+export default function TodoItem({todo, onCompletedChange}: TodoItemProps) {
     return(
         <div>
             <label className="flex items-center gap-2 border rounded-md p-2 border-gray-400 bg-white hover:bg-slate-50">
                 <input
                 type="checkbox"
+                checked={todo.completed}
+                onChange={(e) => onCompletedChange(todo.id, e.target.checked)}
                 className="scale-125"
                 />
                 <span className={todo.completed ? "line-through text-gray-400" : ""}>
