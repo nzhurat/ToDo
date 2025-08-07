d363ae53bfee253d2cb5bfd62aace62a9ee1bb45
nzhurat <zuratnasta2606@icloud.com>
2025-08-07 11:15:22 +0200

    add sorting

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import TodoItem from "./components/TodoItem"
 import { dummyData } from "./data/todos"
 import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState(dummyData)
@@ -28,18 +29,8 @@ function App() {
     <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
-        <AddTodoForm 
-        onSubmit={addTodo}
-        />
-        <div className="space-y-2">
-          {todos.map((todo) => (
-            <TodoItem 
-            key={todo.id}
-             todo={todo} 
-             onCompletedChange={setTodoCompleted}
-            />
-          ))}
-        </div>
+        <AddTodoForm onSubmit={addTodo} />
+        <TodoList todos={todos} onCompletedChange={setTodoCompleted} />
       </div>
     </main>
   )
